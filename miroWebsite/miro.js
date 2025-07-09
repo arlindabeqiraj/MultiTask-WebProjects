616898cd8f0185b19257fe76b03c06645f5a8c4d
Arlinda Beqiraj <arlinda.beqiraj1@student.uni-pr.edu>
2025-07-09 23:13:06 +0200

    Add loading spinner utility function

--- miroWebsite/miro.js
+++ miroWebsite/miro.js
@@ -17,3 +17,16 @@ function showToast(message, type = "success") {
     });
   }, 4000);
 }
+
+// --- Loading Spinner ---
+function showLoading(button, duration = 2000, callback) {
+  const originalText = button.innerHTML;
+  button.disabled = true;
+  button.innerHTML = `<span class="animate-spin mr-2 inline-block w-4 h-4 border-2 border-white border-t-transparent rounded-full"></span>Loading...`;
+
+  setTimeout(() => {
+    button.disabled = false;
+    button.innerHTML = originalText;
+    callback();
+  }, duration);
+}
